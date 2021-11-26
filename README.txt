9416d59eb88cad64637fd5b85f1f2a1c65fef4c1
arda-guler <ardaguler09@gmail.com>
2021-11-26 22:39:20 +0300

    Repo setup

---
+++ README.txt
@@ -0,0 +1,15 @@
+rgb_contraster: assigns max (255) or min (0) values to
+                RGB (and alpha if available) channels
+				depending on whether their current value
+				is higher than half (127).
+				
+rgb_separator:  assigns max value to whichever color is
+                the dominant one in a pixel. if multiple
+				colors are equally dominant, their
+				combination is used.
+				
+rgb_separator_shaded:
+                does what rgb_separator does, but instead
+				of raising the color value to max, it
+				instead raises it to the pixel's brightness
+				value
---
+++ rgb_separator.py
@@ -0,0 +1,105 @@
+from PIL import Image
+
+def import_image():
+    filename = input("Image file name:")
+
+    if not "." in filename:
+        try:
+            image = Image.open(filename + ".png")
+            filename = filename + ".png"
+        except:
+            try:
+                image = Image.open(filename + ".jpg")
+                filename = filename + ".jpg"
+            except:
+                print("Can not access file. Please check file name and file extension!")
+                quit()
+    else:
+        image = Image.open(filename)
+        
+    pixels = image.load()
+
+    filename = filename.split(".")
+
+    if len(filename) == 2:
+        filetitle = filename[0]
+        fileextension = "." + filename[1]
+
+    else:
+        filetitle = filename[0]
+        for i in range(1, len(filename)-1):
+            filetitle += "." + filename[i]
+        fileextension = "." + filename[-1]
+
+    return image, pixels, filetitle, fileextension
+
+
+def main():
+    image, pixels, filetitle, fileextension = import_image()
+
+    channel_num = len(pixels[0,0])
+
+    def separate(r, g, b, a = None):
+        # check alpha
+        if a and a > 127:
+            alpha = 255
+        else:
+            alpha = 0
+
+        # check if one color is greater than others
+        if r > g and r > b:
+            return 255, 0, 0, alpha
+
+        elif g > r and g > b:
+            return 0, 255, 0, alpha
+
+        elif b > r and b > g:
+            return 0, 0, 255, alpha
+
+        # check if two largest colors have equal value
+        elif r == g and r > b:
+            return 255, 255, 0, alpha
+
+        elif r == b and r > g:
+            return 255, 0, 255, alpha
+
+        elif g == b and g > r:
+            return 0, 255, 255, alpha
+
+        # check if all colors have equal value (black, gray or white pixel)
+        elif r == g and g == b:
+            return 255, 255, 255, alpha
+
+    # user comforting variable
+    # very important
+    i = 0
+
+    print("\nApplying RGB separation to " + filetitle + fileextension + "\n")
+
+    # R, G, B channels
+    if channel_num == 3:
+        for y in range(image.size[1]):
+            for x in range(image.size[0]):
+                r, g, b = pixels[x, y][0], pixels[x, y][1], pixels[x, y][2]
+                r, g, b, a = separate(r, g, b)
+                pixels[x, y] = (r, g, b)
+
+                i += 1
+                if i % 5000000 == 0 and i > 0:
+                    print("Still working...")
+        
+    # R, G, B, Alpha channels
+    elif channel_num == 4:
+        for y in range(image.size[1]):
+            for x in range(image.size[0]):
+                r, g, b, a = pixels[x, y][0], pixels[x, y][1], pixels[x, y][2], pixels[x, y][3]
+                r, g, b, a = separate(r, g, b, a)
+                pixels[x, y] = (r, g, b, a)
+
+                i += 1
+                if i % 5000000 == 0 and i > 0:
+                    print("Still working...")
+
+    image.save(filetitle + "_RGBseparate" + fileextension)
+
+main()
